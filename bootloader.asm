21da33597bf63ac6de32862fb7f613e125c344c3
MalekeHan <Malekehanson@gmail.com>
2024-04-12 21:41:43 -0700

    CPU entering a bad state now

--- bootloader.asm
+++ bootloader.asm
@@ -1,52 +1,45 @@
 ; bootloader.asm - A simple bootloader for loading a program into memory and executing it.
 
-;  expects the program's length (in words) to be provided
-; in the input buffer in big-endian byte order
-; followed by the program itself, also in big-endian byte order.
+; Initialize memory address pointer and loop counter
+loadLiteral 1024 r1 ; r1 points to memory address 1024 for storing the program
+loadLiteral 0 r2    ; r2 acts as the loop counter
 
-; read this data from the input device and store it in memory starting at address 1024, then jump to address 1024
-; to execute the program.
+; Read the program length
+read r3             ; Read high byte of program length
+read r4             ; Read low byte of program length
+shl r3 8 r3         ; Shift high byte left to form the high part of the word
+or r3 r4 r5         ; r5 now holds the program length in words
 
-; Set up a register to act as the memory address pointer
-loadLiteral 1024 r1 ; r1 now points to memory address 1024
+; Compute the absolute address for end_read by adding the offset
+loadLiteral 1024 r4 ; Load base address (1024) into r4
+loadLiteral .end_read r6 ; Assuming this is how we get the offset for 'end_read'
+add r4 r6 r4        ; r4 now has the absolute address of 'end_read'
 
-loadLiteral 0 r2 ; this will act like our loop to read how many words of the program has been read and stored
+start_loop:
+    eq r2 r5 r6      ; Compare current count r2 with total length r5
+    cmove r6 r4 r7   ; If r2 == r5, conditionally move the end_read address to r7 to jump there
 
-; read the program length 
-read r3 ; read high byte of program length
-read r4 ; read low byte of program length
-shl r3 8 r3 ; shift high byte
-or r3 r4 r5 ; r5 now contains the program length in words
+    ; Read and store a word from input
+    read r3          ; Read high byte of a word
+    shl r3 24 r3     ; Position high byte
+    read r4
+    shl r4 16 r4
+    or r3 r4 r3      ; Combine high and second byte
+    read r4
+    shl r4 8 r4
+    or r3 r4 r3      ; Combine with third byte
+    read r4
+    or r3 r4 r3      ; r3 now contains the full word
+    store r3 r1      ; Store the word in memory at address pointed by r1
 
-; read the program data into memory starting at address 1024
-read_program:
-    eq r2 r5 r6 ; compare loop counter r2 with program length r5 -- check to see if we have exhausted the loop
-    cmove r6 end_read r7 ; if we are at the end of the loop then we should move the end_read into r7
+    add r1 4 r1      ; Increment the memory address pointer
+    add r2 1 r2      ; Increment the word count
 
-    ; read a word from input and store it at current memory address pointed by r1
-    read r3 ; read high byte of word
-    shl r3 24 r3 ; shift to correct position
-    read r4 ; read second byte of word
-    shl r4 16 r4 ; shift to correct position
-    or r3 r4 r3 ; combine the two bytes
-
-    ; this is the first half of the word done
-
-    read r4 ; Read third byte of word
-    shl r4 8 r4 ; shift to correct position
-    or r3 r4 r3 ; combine
-    read r4 ; read low byte of word
-    or r3 r4 r3 ; r3 now contains the full word
-    store r3 r1 ; store the word in memory at address in r1
-
-    ; Increment memory address and loop counter
-    add r1 4 r1 ; move to the next memory address
-    add r2 1 r2 ; increment the loop counter
-    jmp read_program ; loop back to read next word -- need to replace the jmp instruction with a proper way to restart the loop -- similar to line 89 in prime_embedded.asm
+    ; Calculate the address to continue the loop, considering the base offset
+    loadLiteral 1024 r5       ; Load the base offset
+    add r5 .start_loop r5              ; Adjust the loop start address by adding the base offset
+    move r5 r7                ; Unconditionally set the instruction pointer to loop start
 
 end_read:
-; the program has been read into memory -- we can now execute it 
-loadLiteral 1024 r7 ; put the pointer back to the start of the program
-
-; end of bootloader.asm
-halt 
+    loadLiteral 1024 r7       ; Set the instruction pointer to start executing the program
+    halt                      ; Optionally halt if something follows that should not execute
